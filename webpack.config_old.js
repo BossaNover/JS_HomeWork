768c079329744ee31be036bdbcc030a4a7ce3ce9
vitaly izvekov <bossanover@yandex.ru>
2020-09-21 18:31:51 +0300

    initial commit

--- webpack.config_old.js
+++ webpack.config_old.js
@@ -51,8 +51,7 @@ module.exports = {
         options: { cacheDirectory: true },
       },
       {
-        test: /\.html$/,
-        include: [path.resolve(__dirname, 'projects')],
+        test: /projects\/.+\.html/,
         use: [
           { loader: './scripts/html-inject-loader.js' },
           {
